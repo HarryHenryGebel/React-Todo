7e0c6e87db183f2046320ae7dacc42583485d868
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-03 15:25:47 -0400

    Add state management functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from "react";
 import "fontsource-roboto";
 
-import TodoForm from "./components/TodoForm.js";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 export default class App extends React.Component {
-  // this component is going to take care of state, and any change handlers you need to work with your state
   constructor () {
     super();
+    this.state = {
+      todoList: []
+    };
+  }
+
+  addTodo(todoText) {
+    // create a new TodoItem and add it to the list
+    this.setState(
+      {todoList: [...this.state.todoList, new TodoItem(todoText)]});
+  }
+
+  clearCompleted() {
+    // remove any completed TodoItem instances from state
+    this.setState({todoList: this.state.todoList.filter(
+      (todoItem) => !todoItem.completed
+    )});
   }
 
   render() {
     return (
       <div>
         <h1>Gebeldo</h1>
-        <TodoForm/>
+        <TodoForm addTodo={this.addTodo} clearCompleted={this.clearCompleted}/>
       </div>
     );
   }
 }
+
+//  LocalWords:  TodoForm TodoItem
