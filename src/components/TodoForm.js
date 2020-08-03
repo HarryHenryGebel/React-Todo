9c840ca38f0e0a7bd0f9b2f648897c9fd6115fe3
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-03 14:45:33 -0400

    Convert to stateless function component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -7,53 +7,25 @@ import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 
-const styles = (theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-  button: {
-    margin: theme.spacing(1),
-  },
-});
+export default function TodoForm (props) {
 
-class TodoForm  extends React.Component {
-  constructor() {
-    super();
-
-  }
-
-  render () {
-    const classes = this.props;
-
-    return (
-      <Card className={classes.root} variant="outlined">
-        <h2>Add Todo Item:</h2>
-        <Box m={1}>
-          <TextField variant="filled" label="TODO Item"/>
-        </Box>
-        <br/>
-        <Box m={1} component="span">
-          <Button
-            variant="contained"
-            className={classes.button}
-            startIcon={<AddCircleIcon/>}>
-            Add Todo Item
-          </Button>
-        </Box>
-        <Box m={1} component="span">
-          <Button
-            variant="contained"
-            className={classes.button}
-            startIcon={<HighlightOffIcon/>}>
-            Clear completed items
-          </Button>
-        </Box>
-      </Card>
-    );
-  }
+  return (
+    <Card variant="outlined">
+      <h2>Add Todo Item:</h2>
+      <Box m={1}>
+        <TextField fullWidth="true" variant="filled" label="TODO Item"/>
+      </Box>
+      <br/>
+      <Box m={1} component="span">
+        <Button variant="contained" startIcon={<AddCircleIcon/>}>
+          Add Todo Item
+        </Button>
+      </Box>
+      <Box m={1} component="span">
+        <Button variant="contained" startIcon={<HighlightOffIcon/>}>
+          Clear completed items
+        </Button>
+      </Box>
+    </Card>
+  );
 }
-
-export default withStyles(styles, { withTheme: true })(TodoForm);
