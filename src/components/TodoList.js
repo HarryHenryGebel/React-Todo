8522f34ec8041a97e9bd798e1b51759b6c6beba2
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-03 09:00:14 -0400

    Revert TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,2 @@
 // your components will all go in this `component` directory.
-pnnnp// feel free to change this component.js into TodoList.js
+// feel free to change this component.js into TodoList.js
