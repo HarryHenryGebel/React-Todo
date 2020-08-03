912ff796f021774b879e9069666c41310283b8f9
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-03 16:48:37 -0400

    TodoForm local state functional

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,31 +1,58 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
 import TextField from "@material-ui/core/TextField";
-import { withStyles } from "@material-ui/core/styles";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 
-export default function TodoForm (props) {
+export default class TodoForm extends React.Component {
+  constructor (props) {
+    super();
 
-  return (
-    <Card variant="outlined">
-      <h2>Add Todo Item:</h2>
-      <Box m={1}>
-        <TextField fullWidth="true" variant="filled" label="TODO Item"/>
-      </Box>
-      <br/>
-      <Box m={1} component="span">
-        <Button variant="contained" startIcon={<AddCircleIcon/>}>
-          Add Todo Item
-        </Button>
-      </Box>
-      <Box m={1} component="span">
-        <Button variant="contained" startIcon={<HighlightOffIcon/>}>
-          Clear completed items
-        </Button>
-      </Box>
-    </Card>
-  );
+    this.addTodo = props.addTodo;
+    this.clearCompleted = props.clearCompleted;
+    this.state = {todoText: ""};
+  }
+
+  submitAndClear = () => {
+      this.addTodo(this.state.todoText);
+//    this.setState()
+  }
+
+  textChange = (event) => {
+    this.setState({todoText: event.target.value});
+  }
+
+  render () {
+    return (
+      <Card variant="outlined">
+        <h2>Add Todo Item:</h2>
+        <Box m={1}>
+          <TextField fullWidth={true}
+                     id="todo-item-field"
+                     onChange={this.textChange}
+                     value={this.state.todoText}
+                     variant="filled"
+                     label="Todo Item"/>
+        </Box>
+        <br/>
+        <Box m={1} component="span">
+          <Button variant="contained"
+                  disabled={this.state.todoText === ""}
+                  startIcon={<AddCircleIcon/>}
+                  onClick={this.submitAndClear}>
+            Add Todo Item
+          </Button>
+        </Box>
+        <Box m={1} component="span">
+          <Button variant="contained"
+                  onClick={this.clearCompleted}
+                  startIcon={<HighlightOffIcon/>}>
+            Clear completed items
+          </Button>
+        </Box>
+      </Card>
+    );
+  }
 }
