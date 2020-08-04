7bbba4a3727a9726049236cb3db4a9c9c22a4bf7
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-04 00:40:05 -0400

    Render Todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+export default function Todo(props) {
+  const {todoData} = props;
+
+  return (
+        <ListItem button>
+          <ListItemText primary={todoData.task} />
+        </ListItem>
+  );
+}
+
+//  LocalWords:  ListItem ListItemText
