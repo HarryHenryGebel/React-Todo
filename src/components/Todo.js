dd717b8d53c0c1df90b6f05ea8efac07d6283435
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-04 01:24:47 -0400

    Complete MVP

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,16 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
 export default function Todo(props) {
-  const {todoData} = props;
+  const {todoData, toggleCompleted} = props,
+        // strike-through completed items
+        lineStyle = todoData.completed ?
+        {textDecoration: "line-through"} :
+        {textDecoration: "none"};
 
   return (
-        <ListItem button>
-          <ListItemText primary={todoData.task} />
+    <ListItem button onClick={() => toggleCompleted(todoData)}>
+          <ListItemText primary={todoData.task}
+                        primaryTypographyProps={{style: lineStyle}}/>
         </ListItem>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,12 +4,14 @@ import List from '@material-ui/core/List';
 import Todo from "./Todo";
 
 export default function TodoList (props) {
-  const {todoList} = props;
+  const {todoList, toggleCompleted} = props;
 
   return (
     <List>
       {todoList.map(todoData => (
-        <Todo todoData={todoData}/>
+        <Todo key={todoData.id}
+              todoData={todoData}
+              toggleCompleted={toggleCompleted}/>
       ))}
     </List>
   );
